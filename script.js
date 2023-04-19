4d101f700b721f698dc7826b528c6ab935e06ad3
Redaska <reda.essakali@gmail.com>
2023-04-19 13:05:14 +0000

    Revert "Change the variables name and add ask for a choice"
    This reverts commit 5e3666cbbdfdeac79160ac9fe773430591e25d6f.

--- script.js
+++ script.js
@@ -2,19 +2,16 @@
 let num = Math.floor(Math.random() * 3 + 1);
 console.log(num);
 //now since it works we make a function with an if else if statements
-//ps i made the computerSelection in the global scope so i can call it for the other function
-let computerSelection;
+//ps i made the computerChoice in the global scope so i can call it for the other function
+let computerChoice;
 function getComputerChoice() {
   if (num === 1) {
-    return (computerSelection = "rock");
+    return (computerChoice = "Rock");
   } else if (num === 2) {
-    return (computerSelection = "paper");
+    return (computerChoice = "Paper");
   } else {
-    return (computerSelection = "scissor");
+    return (computerChoice = "Scissor");
   }
 }
 getComputerChoice();
-
-//lets ask the person to enter his choice
-let choice = prompt("I wonder what you will choose", "Rock-Paper-Scissor");
-let playerSelection = choice.toLowerCase();
+console.log(computerChoice);
