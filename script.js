5c7015c84e0bf404022a03f96b2c0c0ef806296e
Redaska <reda.essakali@gmail.com>
2023-04-24 23:00:45 +0100

    Display the score and end the game on 5 rounds

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 
 //now since it works we make a function with an if else if statements
 //ps i made the computerSelection in the global scope so i can call it for the other function
+let computerScore = 0;
+let playerScore = 0;
 let computerSelection;
+let playerSelection = "";
+
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3 + 1);
   if (num === 1) {
@@ -46,13 +50,16 @@ function playerChoice() {
 function round(x, y) {
   getComputerChoice();
   let result = document.querySelector("#result");
-
+  let computerScoreDis = document.querySelector(".computer-scr");
+  let playerScoreDis = document.querySelector(".player-scr");
   if (
     (x == "rock" && y == "scissor") ||
     (x == "paper" && y == "rock") ||
     (x == "scissor" && y == "paper")
   ) {
     result.textContent = "Agh you won this round";
+    playerScore++;
+    playerScoreDis.textContent = playerScore;
   } else if (x === y) {
     result.textContent = "A draw ? How boring!";
   } else if (
@@ -60,11 +67,33 @@ function round(x, y) {
     (y == "paper" && x == "rock") ||
     (y == "scissor" && x == "paper")
   ) {
-    result.textContent = "Obviously i win!";
+    result.textContent = "HAHAHAHAH! Obviously i win!";
+    computerScore++;
+    computerScoreDis.textContent = computerScore;
   }
+  if (computerScore == 5 || playerScore == 5) {
+    endGame();
+  }
+}
+function endGame() {
+  if (playerScore === 5) {
+    result.textContent = "HOW? ME ? LOSE ? NOOOOOOOOOOO!";
+    disableButtons();
+  } else if (computerScore === 5) {
+    result.textContent = "U NEVER HAD A CHANCE TO BEGINE WITH HAHAHAHAH";
+    disableButtons();
+  }
+}
+
+function disableButtons() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
 }
 
 //the round function is just for one round we need a game function it should be a loop of 5 rounds
 // i still haven't learnt how to make a loop bur Mr.google can help
-let playerSelection = "";
+//well now i did  but i ll make a condition for this one
+
 playerChoice();
