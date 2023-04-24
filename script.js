99a8b6d6621de96f8778ca7fa3f3c049f6d0129f
Redaska <reda.essakali@gmail.com>
2023-04-24 17:11:39 +0100

    Let the buttons get the playerSelection value
    i also fixed some placement problems within the code to get a random number each round

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissor = document.querySelector("#scissors");
 //use something that will generate a random int between 1-3 each number represents a word .
-let num = Math.floor(Math.random() * 3 + 1);
-console.log(num);
+
 //now since it works we make a function with an if else if statements
 //ps i made the computerSelection in the global scope so i can call it for the other function
 let computerSelection;
 function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3 + 1);
   if (num === 1) {
     return (computerSelection = "rock");
   } else if (num === 2) {
@@ -16,13 +13,32 @@ function getComputerChoice() {
     return (computerSelection = "scissor");
   }
 }
-getComputerChoice();
-console.log(computerSelection);
 
 //lets ask the person to enter his choice
-let choice = prompt("I wonder what you will choose", "Rock-Paper-Scissor");
-let playerSelection = choice.toLowerCase();
-console.log(playerSelection);
+getComputerChoice();
+function playerChoice() {
+  const rock = document.getElementById("rock");
+  const paper = document.getElementById("paper");
+  const scissors = document.getElementById("scissors");
+
+  rock.addEventListener("click", () => {
+    playerSelection = "rock"; // set playerSelection to "rock"
+    console.log(playerSelection); // log playerSelection to the console
+    round(playerSelection, computerSelection);
+  });
+
+  paper.addEventListener("click", () => {
+    playerSelection = "paper"; // set playerSelection to "paper"
+    console.log(playerSelection); // log playerSelection to the console
+    round(playerSelection, computerSelection);
+  });
+
+  scissors.addEventListener("click", () => {
+    playerSelection = "scissor"; // set playerSelection to "scissors"
+    console.log(playerSelection); // log playerSelection to the console
+    round(playerSelection, computerSelection);
+  });
+}
 
 //lets make a function that tells us who won
 
@@ -33,20 +49,20 @@ function round(x, y) {
     (x == "paper" && y == "rock") ||
     (x == "scissor" && y == "paper")
   ) {
-    return console.log((result = "Agh you won this round"));
+    result = "Agh you won this round";
   } else if (x === y) {
-    return console.log((result = "A draw ? How boring!"));
+    result = "A draw ? How boring!";
   } else if (
     (y == "rock" && x == "scissor") ||
     (y == "paper" && x == "rock") ||
     (y == "scissor" && x == "paper")
   ) {
-    return console.log((result = "Obviously i win!"));
-  } else {
-    return console.log((result = "HAHAHAHAHA you cant even enter an option"));
+    result = "Obviously i win!";
   }
+  console.log(result);
 }
-round(playerSelection, computerSelection);
 
 //the round function is just for one round we need a game function it should be a loop of 5 rounds
 // i still haven't learnt how to make a loop bur Mr.google can help
+let playerSelection = "";
+playerChoice();
